31fece6ee925e3c7ad5681e878d417451b5a9be5
carchacq <n.karshak@gmail.com>
2022-12-09 11:09:40 +0300

    fixed

--- main.py
+++ main.py
@@ -1,56 +1,48 @@
 # 1. Итератор
 
-# class FlatIterator:
-#
-#     def __init__(self, list_of_list):
-#         self.list_of_list = list_of_list
-#         pass
-#
-#     def __iter__(self):
-#         self.ind_out = 0
-#         return self
-#
-# # Этот код не работает. Не понимаю, почему. Ведь StopIteration происходит, когда все списки уже пустые
-#     # def __next__(self):
-#     #     item = self.list_of_list[self.ind_out].pop(self.ind_in)
-#     #     if len(self.list_of_list[self.ind_out]) == 0:
-#     #         self.ind_out += 1
-#     #     if self.ind_out == len(self.list_of_list):
-#     #         raise StopIteration
-#     #     return item
-#
-#     def __next__(self):
-#         while self.ind_out != len(self.list_of_list):
-#             item = self.list_of_list[self.ind_out].pop(0)
-#             if len(self.list_of_list[self.ind_out]) == 0:
-#                 self.ind_out += 1
-#             return item
-#
-#         raise StopIteration
-#
-# def test_1():
-#     import copy
-#     list_of_lists_1 = [
-#         ['a', 'b', 'c'],
-#         ['d', 'e', 'f', 'h', False],
-#         [1, 2, None]
-#     ]
-#     copy1 = copy.deepcopy(list_of_lists_1)
-#     copy2 = copy.deepcopy(list_of_lists_1)
-#
-#     for flat_iterator_item, check_item in zip(
-#             FlatIterator(copy1),
-#             ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
-#     ):
-#
-#         assert flat_iterator_item == check_item
-#
-#
-#     assert list(FlatIterator(copy2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
-#
-#
-# if __name__ == '__main__':
-#     test_1()
+class FlatIterator:
+
+    def __init__(self, list_of_list):
+        self.lists = copy.deepcopy(list_of_list)
+        pass
+
+    def __iter__(self):
+        self.ind_out = 0
+        return self
+
+
+
+    def __next__(self):
+        while self.ind_out != len(self.lists):
+            item = self.lists[self.ind_out].pop(0)
+            if len(self.lists[self.ind_out]) == 0:
+                self.ind_out += 1
+            return item
+
+        raise StopIteration
+
+def test_1():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+
+
+if __name__ == '__main__':
+    import copy
+    test_1()
 
 
 
